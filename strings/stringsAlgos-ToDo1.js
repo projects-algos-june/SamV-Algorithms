85b3821ac9f376cff65727248375b1fa0351f7c7
BigSamu <valdesgutierrez@gmail.com>
2020-06-05 20:45:58 -0400

    To Do 1 - Strings

--- strings/stringsAlgos-ToDo1.js
+++ strings/stringsAlgos-ToDo1.js
@@ -6,25 +6,29 @@
 // Excercise 1
 
 console.log("\n"+"Excercise 1");
-removeBlanks(" Pl ayTha tF u nkyM usi c ")// Should return "PlayThatFunkyMusic
+removeBlanks(" Pl ayTha tF u nkyM usi c ") // Should return "PlayThatFunkyMusic"
 
 // Excercise 2
 
 console.log("\n"+"Excercise 2");
-getDigits("0s1a3y5w7h9a2t4?6!8?0")
+getDigits("0s1a3y5w7h9a2t4?6!8?0") // Should return "01357924680"
 
 // Excercise 3
 
 console.log("\n"+"Excercise 3");
+acronym(" there's no free lunch - gotta pay yer way. "); // should return "TNFL-GPYW"
+acronym("Live from New York, it's Saturday Night!") // should return "LFNYISN"
 
 // Excercise 4
 
 console.log("\n"+"Excercise 4");
+countNonSpaces("Honey pie, you are driving me crazy") // should return 29
 
 
 // Excercise 5
 
 console.log("\n"+"Excercise 5");
+removeShorterStrings(["Today","is","a","great","day!"], 3) // should return ["Today", "great", "day!"]
 
 
 // =========================================================================================
@@ -46,17 +50,20 @@ function removeBlanks(aString){
 // Excercise 2
 
 // Get Digits
-// Create a JavaScript function that given a string, returns the integer made from the string’s digits. Given "0s1a3y5w7h9a2t4?6!8?0", the function should return the number 1357924680.
+// Create a JavaScript function that given a string, returns the integer made from the string’s digits. Given "0s1a3y5w7h9a2t4?6!8?0", the function should return the number 01357924680.
 
 function getDigits(aString){
-    charsArray = aString.split('')
-    digitsArray = []
+    var charsArray = aString.split('')
+    var digitsArray = []
+    
     for (i =0; i < charsArray.length; i++){
-        if (isNumber(charsArray[i])){
+        if (!isNaN(charsArray[i])){
             digitsArray.push(charsArray[i])  
         }
     }
-    return false;
+    var digits = digitsArray.join('')
+    console.log(digits)
+    return digits;
 }
 
 
@@ -67,6 +74,16 @@ function getDigits(aString){
 // Given " there's no free lunch - gotta pay yer way. ", return "TNFL-GPYW". 
 // Given "Live from New York, it's Saturday Night!", return "LFNYISN".
 
+function acronym(aString){
+    var stringArray = aString.split(' ')
+    var acronymArray = []
+    for (i =0; i < stringArray.length; i++){
+        acronymArray.push(stringArray[i].toUpperCase()[0])  
+    }
+    var acronym = acronymArray.join('')
+    console.log(acronym)
+    return acronym;
+}
 
 // Excercise 4
 
@@ -74,12 +91,35 @@ function getDigits(aString){
 // Accept a string and return the number of non-space characters found in the string. 
 // For example, given "Honey pie, you are driving me crazy", return 29 (not 35).
 
-
+function countNonSpaces(aString){
+    var counter = 0
+    for (i =0; i < aString.length; i++){
+        if (aString[i] != " "){
+            counter++
+        } 
+    }
+    console.log(counter)
+    return counter;
+}
 
 // Excercise 5
 
 // Remove Shorter Strings
 // Given a string array and value (length), remove any strings shorter than the length from the array.
+
+function removeShorterStrings(stringArray, minLength){
+
+    for (var i = stringArray.length-1; i >=0; i--){
+        if (stringArray[i].length < minLength){
+            for (var j = i; j < stringArray.length-1; j++){
+                [stringArray[j],stringArray[j+1]] = [stringArray[j+1],stringArray[j]]
+            }
+            stringArray.pop()
+        } 
+    }
+    console.log(stringArray)
+    return stringArray;
+}
 
 
 
